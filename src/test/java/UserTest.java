e4f49383358e4fc3096c9736c01eb75fadf0d031
liuwei <wei.liu@kiwinano.com>
2018-09-25 10:59:08 +0800

    test

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,19 @@
+import com.hector.mappers.UserMapper;
+import com.hector.models.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserTest extends BaseTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void allUser(){
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.all();
+        userList.forEach(System.out::println);
+    }
+}
