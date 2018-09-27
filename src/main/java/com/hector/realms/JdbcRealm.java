b33c261a4b8508c964dcdbcb534b55ad2df11abc
liuwei <wei.liu@kiwinano.com>
2018-09-27 18:27:08 +0800

    +) add shiro

---
+++ src/main/java/com/hector/realms/JdbcRealm.java
@@ -0,0 +1,24 @@
+package com.hector.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+public class JdbcRealm implements Realm {
+    @Override
+    public String getName() {
+        return "JdbcRealm";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        return null;
+    }
+}
